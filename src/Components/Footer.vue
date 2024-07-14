a7ac25010a5ad3769ce9e4f1e303cd2e612ea330
test <test@MBP-Malik.lan>
2024-07-15 00:48:26 +0500

    finish-2

--- src/Components/Footer.vue
+++ src/Components/Footer.vue
@@ -2,16 +2,20 @@
 
 <template>
   <footer class="bg-[#3f3f46]">
-    <div class="flex justify-between w-10/12">
+    <div class="flex justify-between mx-auto w-10/12">
       <div class="w-8/12 mx-auto flex gap-2 items-center h-20 justify-start">
         <h3 class="text-xl mt-1 text-orange-500 self-start items-start uppercase font-light">
           Сайт разработан:
         </h3>
         <div class="flex flex-col gap-4">
-          <a class="text-white text-xl uppercase font-thin" href="tel:+77025379473"
+          <a
+            class="text-white text-xl whitespace-nowrap max-sm:text-sm uppercase font-thin"
+            href="tel:+77025379473"
             >+7 702 537 9473</a
           >
-          <a class="text-white text-xl uppercase font-thin" href="tel:+77025379473"
+          <a
+            class="text-white whitespace-nowrap max-sm:text-sm text-xl uppercase font-thin"
+            href="tel:+77025379473"
             >+7 707 486 2082</a
           >
         </div>
